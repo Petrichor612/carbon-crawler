351366b18e256ea4942b3dd51eb5d9e54d9e2783
Petrichor612 <87970583+Petrichor612@users.noreply.github.com>
2026-04-17 17:29:14 +0800

    Update CarbonPolicyApplication.java

--- carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
+++ carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
@@ -1,18 +1,17 @@
 package com.carbon;
 
-/**
- * @author zhangbo $
- * @title $
- * @description $
- * @updateTime $ 11:53$ $
- * @throws $
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableScheduling
+// 关键：只扫描 controller，其他全部不加载！
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
+@ComponentScan(basePackages = "com.carbon.controller") // 只加载接口！
 public class CarbonPolicyApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarbonPolicyApplication.class, args);
