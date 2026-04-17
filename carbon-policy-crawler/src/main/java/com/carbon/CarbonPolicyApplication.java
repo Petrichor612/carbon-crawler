74ed6f73a530efce7b8e61d8f0af8e7e395d2462
Petrichor612 <87970583+Petrichor612@users.noreply.github.com>
2026-04-17 14:55:14 +0800

    Update CarbonPolicyApplication.java

--- carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
+++ carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
@@ -1,20 +1,21 @@
 package com.carbon;
 
-/**
- * @author zhangbo $
- * @title $
- * @description $
- * @updateTime $ 11:53$ $
- * @throws $
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-// @EnableScheduling
-public class CarbonPolicyApplication {
+public class CarbonPolicyCrawlerApplication {
+
+    // 直接把首页写在启动类里！100% 被加载！
+    @GetMapping("/")
+    public String home() {
+        return "✅ 项目启动成功！公网运行正常！";
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(CarbonPolicyApplication.class, args);
+        SpringApplication.run(CarbonPolicyCrawlerApplication.class, args);
     }
 }
