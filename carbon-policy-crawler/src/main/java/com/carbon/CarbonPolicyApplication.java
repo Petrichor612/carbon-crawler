4e928adddb9f0cb024912a32892b7ce76a887269
Petrichor612 <87970583+Petrichor612@users.noreply.github.com>
2026-04-17 18:01:23 +0800

    Update CarbonPolicyApplication.java

--- carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
+++ carbon-policy-crawler/src/main/java/com/carbon/CarbonPolicyApplication.java
@@ -10,9 +10,11 @@ package com.carbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("com.carbon.mapper")
 public class CarbonPolicyApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarbonPolicyApplication.class, args);
